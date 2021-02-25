b90e067a632452bd08adbd48782d460b7c31c135
zachary-light <78107535+zachary-light@users.noreply.github.com>
2021-02-25 13:37:11 -0600

    Update Quik-Ventory-Database1.0.sql

--- Capstone-Project/Project-Code/Quik-Ventory-Database1.0.sql
+++ Capstone-Project/Project-Code/Quik-Ventory-Database1.0.sql
@@ -2,19 +2,20 @@ create database Quik_Ventory;
 use Quik_Ventory;
 
 create table users(
-    email varchar(35),
+    email varchar(35) UNIQUE,
     username varchar(35) NOT NULL,
+    user_ID integer AUTO_INCREMENT,
     password varchar(30) NOT NUll,
-    primary key (email)
+    primary key (user_ID)
 );
 
 create table inventory(
-    email varchar(35),
+    user_ID integer,
     item_ID varchar(15), /*barcode: UPC standard is 12 digits*/
     item_name varchar(30) NOT NULL,
     qty integer NOT NULL,
     price decimal(7,2),
     dept varchar(30),
-    primary key (email, item_ID),
-    foreign key (email) references users (email)
+    primary key (user_ID, item_ID),
+    foreign key (user_ID) references users (user_ID)
 );
