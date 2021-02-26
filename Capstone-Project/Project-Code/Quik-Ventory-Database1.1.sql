e2b789f3d0d3925024e540d54b1f07c63fb805a9
zachary-light <78107535+zachary-light@users.noreply.github.com>
2021-02-25 18:32:50 -0600

    Add files via upload

---
+++ Capstone-Project/Project-Code/Quik-Ventory-Database1.1.sql
@@ -0,0 +1,20 @@
+use quikvory_Quik_Ventory;
+
+create table users(
+    email varchar(35) UNIQUE,
+    username varchar(35) NOT NULL,
+    user_ID integer AUTO_INCREMENT,
+    password varchar(30) NOT NUll,
+    primary key (user_ID)
+);
+
+create table inventory(
+    user_ID integer,
+    item_ID varchar(15), /*barcode: UPC standard is 12 digits*/
+    item_name varchar(30) NOT NULL,
+    qty integer NOT NULL,
+    price decimal(7,2),
+    dept varchar(30),
+    primary key (user_ID, item_ID),
+    foreign key (user_ID) references users (user_ID)
+);
